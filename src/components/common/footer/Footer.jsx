4e35ef3a6437ead48b24b0f5ba7202aad6423ede
Jaimin2203 <jaiminchahun6667@gmail.com>
2024-03-20 10:57:51 +0530

    Error is Fixed the code .

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,39 +1,42 @@
-import React from "react"
-import { footer } from "../../data/Data"
-import "./footer.css"
+import React from "react";
+import { footer } from "../../data/Data";
+import "./footer.css";
 
 const Footer = () => {
   return (
     <>
-      <section className='footerContact'>
-        <div className='container'>
-          <div className='send flex'>
-            <div className='text'>
+      <section className="footerContact">
+        <div className="container">
+          <div className="send flex">
+            <div className="text">
               <h1>Do You Have Questions ?</h1>
               <p>We'll help you to grow your career and growth.</p>
             </div>
-            <button className='btn5'>Contact Us Today</button>
+            <button className="btn5">Contact Us Today</button>
           </div>
         </div>
       </section>
 
       <footer>
-        <div className='container'>
-          <div className='box'>
-            <div className='logo'>
-              <img src='../images/logo-light.png' alt='' />
+        <div className="container">
+          <div className="box">
+            <div className="logo">
+              <img src="../images/logo-light.png" alt="" />
               <h2>Do You Need Help With Anything?</h2>
-              <p>Receive updates, hot deals, tutorials, discounts sent straignt in your inbox every month</p>
+              <p>
+                Receive updates, hot deals, tutorials, discounts sent straignt
+                in your inbox every month
+              </p>
 
-              <div className='input flex'>
-                <input type='text' placeholder='Email Address' />
+              <div className="input flex">
+                <input type="text" placeholder="Email Address" />
                 <button>Subscribe</button>
               </div>
             </div>
           </div>
 
           {footer.map((val) => (
-            <div className='box'>
+            <div className="box">
               <h3>{val.title}</h3>
               <ul>
                 {val.text.map((items) => (
@@ -44,11 +47,11 @@ const Footer = () => {
           ))}
         </div>
       </footer>
-      <div className='legal'>
-        <span>© 2021 RentUP. Designd By GorkCoder.</span>
+      <div className="legal">
+        <span>© 2024 RentUP. Designd By Jaimin.</span>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
